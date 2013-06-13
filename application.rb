9fe9a3efb8e9b71d60deeb9d018edf6c4e14fe70
Austin Lee <austin@futureadvisor.com>
2013-06-12 21:40:24 -0700

    Change sugar to RESTFul and simplify application, doesn't do anything now

--- application.rb
+++ application.rb
@@ -3,28 +3,34 @@ require 'bundler'
 
 Bundler.require
 
-@@confections = []
+@@confections = [{:amount => 5}]
 
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
-get '/sugars' do
+get '/sugar' do
   content_type :json
-  {:models => [{:sugar => 2}]}.to_json
+  @@confections.to_json
 end
 
-post '/sugars' do
-  content_type :json
-  @@confections << :sugar
+get '/sugar/:id' do
+  logger.info params[:id]
+  return @@confections.first.to_json
 end
 
-get '/creams' do
+post '/sugar' do
+  content_type :json
+  logger.info params
+  @@confections << { :sugar => 4}
+end
+
+get '/cream' do
   content_type :json
   {:sugar => 3}.to_json
 end
 
-post '/creams' do
+post '/cream' do
   content_type :json
   @@confections << :cream
 end
--- public/application.js
+++ public/application.js
@@ -1,25 +1,16 @@
 $(document).ready(function() {
 
-  var Sugar = Backbone.Model.extend({});
-  var Cream = Backbone.Model.extend({});
-
-  var SugarList = Backbone.Collection.extend({
-    model: Sugar,
-    url: 'http://localhost:4567/sugars'
+  var Sugar = Backbone.Model.extend({
+    url: 'http://localhost:4567/sugar'
   });
-
-  var CreamList = Backbone.Collection.extend({
-    model: Cream,
-    url: 'http://localhost:4567/creams'
+  var Cream = Backbone.Model.extend({
+    url: 'http://localhost:4567/cream'
   });
-
-  var Sugars = new SugarList;
-  var Creams = new CreamList;
 
   var SugarView = Backbone.View.extend ({
+    template: _.template('<h3>Sugars: <%= amount %></h3>'),
     render: function() {
-      console.log("SugarView render called");
-      this.$el.html( "Hello" );
+      this.$el.html( this.template(this.model.toJSON()) );
       return this;
     }
   });
@@ -29,45 +20,8 @@ $(document).ready(function() {
 
   var AppView = Backbone.View.extend({
 
-    el: $("body"),
-
-    initialize: function() {
-      this.result = "Hello World";
-      Sugars.fetch();
-      console.log(Sugars.toJSON());
-    },
-
     render: function() {
-      console.log("appview Render called");
-      /*var data = Sugars.map( function(sugar){
-        console.log("inside map");
-        var view = new SugarView({model: sugar});
-        return view.render().el;
-      } );
-
-      console.log("appviewfinishmap");
-      var result = data.reduce( function( a,b ) {return a + b}, '' );
-      console.log("appViewfinish reduce");
-      */
-     console.log("Outputting sugars");
-     Sugars.each( function(sugar) {
-       console.log(sugar);
-     } );
-      $('#coffeeStatus').html(this.result);
-      console.log("appview Render finishing");
       return this;
-    },
-
-    addOne: function( sugar ) {
-      console.log(sugar)
-      var view = new SugarView({model: sugar});
-      this.$('#coffeeStatus').append(view.render().el);
-      console.log("addOne called");
-    },
-
-    addAll: function() {
-      Sugars.each(this.addOne, this);
-      console.log("addAll called");
     }
 
   });
